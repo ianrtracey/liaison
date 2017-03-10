82fe2952c87663b5ec11c276efb021c2c91d640d
Ian Tracey <ianrtracey@gmail.com>
2017-03-09 18:56:18 -0700

    Installs Express

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-'use strict'
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-const Config = require('./config.js');
-var FB = require('./connectors/facebook')
-// var Bot = require('./bot')
-app.set('port', (process.env.PORT || 5000))
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+var app = express();
 
-// Process application/json
-app.use(bodyParser.json())
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a Liasion')
-})
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/webhooks', function (req, res) {
-  if (req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send('Error, wrong token')
-})
+app.use('/', index);
+app.use('/users', users);
 
-// for Facebook verification
-app.post('/webhook/', function (req, res) {
-  var entry = FB.getMessageEntry(req.body)
-  if (entry && entry.message) {
-    if (entry.message.attachments) {
-      FB.newMessage(entry.sender.id, "I can't read attachments yet!")
-    } else {
-      FB.newMessage(entry.sender.id, 'cool, bro');
-    }
-  }
-  res.sendStatus(200)
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
